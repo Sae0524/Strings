b1a7ef3c1d71ad8d3051552e82b5866d6b1b1afa
Sae0524 <ume.m3s@gmail.com>
2021-03-18 12:23:35 -0700

    firstcommit

---
+++ strings.js
@@ -0,0 +1,82 @@
+// Implement the following functions
+// your code. Refactor this file as you see fit. Export or modify any function as you see fit. Add additonal functions as you see fit. 
+
+
+
+
+
+
+// Append two strings together, by taking the first string, calling concatanate on it, and giving it the value b
+function concatanate(a,b)
+    {
+    // set up a temp variable
+    let aResult = a;
+    aResult.concatanate(b);
+    return aResult;
+    }
+
+
+
+//https://www.w3schools.com/charsets/ref_html_ascii.asp
+// How to do it: https://www.programiz.com/javascript/examples/ascii-value-character
+function ASCIINumberToChar(Number)
+{
+
+
+}
+
+
+//https://www.w3schools.com/charsets/ref_html_ascii.asp
+// How to do it: https://www.programiz.com/javascript/examples/ascii-value-character
+function CharToAsciiNumber(aChar)
+{
+// take one Character, say, 'A' and convert it to it's Number representation
+}
+
+
+//https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
+function ReverseString(origionalString)
+{
+// return a string, ie: Hello becomes olleH
+}
+
+
+// https://www.w3schools.com/jsref/jsref_toUpperCase.asp
+function GenerateOnlyUpperCase()
+{
+//Ie: A,B,C,D,E,F,G...Z
+}
+
+https://www.w3schools.com/jsref/jsref_toLowerCase.asp
+function GenerateOnlyLowerCase()
+{
+//Ie: a,b,c,d,e,f,g...z
+}
+
+// Make a loop, and show only numbers, 0 to 9 and return that in a list
+function GenerateOnlyNumbers()
+{
+//Ie: 0,1,2,3...9
+}
+
+function GenerateOnlySymbols()
+{
+//Ie: $, %, !, #, etc
+}
+
+
+function GenerateUpperAndLowerCase()
+{
+    // make a list
+    //GenerateOnlyLowerCase
+    //GenerateOnlyUpperCase
+    //return all of that data
+    //Ie: A,B,C,D,E,F,G...Z and  a,b,c,d,e,f,g...z
+}
+
+
+
+const LowerCaseLetters = stringLib.GenerateOnlyLowerCase();
+const UpperCaseLetters = stringLib.GenerateOnlyUpperCase();
+const OnlyNumbers = stringLib.GenerateOnlyNumbers();
+const GenerateOnlySymbols = stringLib.GenerateOnlySymbols();
